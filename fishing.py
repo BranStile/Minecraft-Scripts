1f08666631ae8b8d58740ac06f1d6a5c7fdc04b7
Brandon Stile <bgstile06@gmail.com>
2026-09-05 02:18:24 -0400

    Renamed an image and added fishing pole breakage protection

--- fishing.py
+++ fishing.py
@@ -3,19 +3,30 @@ import time
 
 screenWidth, screenHieght = pyautogui.size()
 
+time.sleep(5)
 
 while(True):
     mousePositionX, mousePositionY = pyautogui.position()
     pixelColor = pyautogui.pixel(mousePositionX, mousePositionY)
     try:
-        area = pyautogui.locateOnScreen("images/image.png", confidence=0.9)
+        pyautogui.locateOnScreen("images/fishingMessage.png", confidence=0.9, grayscale=True)
         pyautogui.rightClick()
         time.sleep(3)
         pyautogui.rightClick()
-    except:
-        pass
+    except:    
+        try:
+            pyautogui.locateOnScreen("images/fishingPoleCasted.png", confidence=0.65, grayscale=True)
+            print("pole")
+        except:
+            pyautogui.press('esc')
+            break
         
+
     time.sleep(.5)
+
+
+
+    
         
     
     
